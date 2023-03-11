38d390e199c07cf03140c2f4f0e2d036b68aad9b
Howard <howardsoon@gmail.com>
2023-03-11 23:24:24 +0000

    complete

---
+++ src/Restaurant.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Order from "./Order"
+
+const Restaurant = () => {
+
+  return (
+    <div className="restaurantOrders">
+      <h3>Restaurant Orders</h3>
+      <ul className="ul-wrapper">
+      <Order orderType = {"Pizzas"}/>
+      <Order orderType = {"Salads"}/>
+      <Order orderType = {"Chocolate cake"}/>
+      </ul>
+    </div>
+  );
+};
+
+export default Restaurant;
---
+++ src/Search.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import SearchButton from "./SearchButton";
+
+const Search = (props) => {
+
+  const [searchInput, setsearchInput] = useState ('')
+
+  function handleSearchInput (event){
+    setsearchInput(event.target.value)
+  }
+  
+  function handleSubmit (e) {
+    e.preventDefault()
+    props.search(e.currentTarget.elements.customerName.value)
+  }
+
+  return (
+    <div className="search">
+      <div className="page-header">
+        <h4 className="text-left">Search Bookings</h4>
+      </div>
+      <div className="row search-wrapper">
+        <div className="col">
+          <form className="form-group search-box" onSubmit = {handleSubmit}>
+            <label htmlFor="customerName">Customer name</label>
+            <div className="search-row">
+              <input
+                onChange={handleSearchInput}
+                value = {searchInput}
+                type="text"
+                id="customerName"
+                className="form-control"
+                placeholder="Customer name"
+              />
+              <SearchButton />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
